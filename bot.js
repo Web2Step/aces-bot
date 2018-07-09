9940808ec95d9b77636614f1e90bafd1d30a8b14
web2step <web2step@mail.ru>
2018-07-09 08:31:17 +0700

    cxcx

--- bot.js
+++ bot.js
@@ -1,25 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-
-function gettext(url) {
-var http = require('http');
-var options = {
-    host: url,
-    path: '/'
-}
-var request = http.request(options, function (res) {
-    var data = '';
-    res.on('data', function (chunk) {
-        data += chunk;
-    });
-    res.on('end', function () {
-        console.log(data);
-    });
-}); 
- request.end();
- return data;
-}
  
 client.on('ready', () => {
     console.log('I am ready!');
@@ -32,8 +13,27 @@ client.on('message', message => {
     if (message.content === '!farm') {
         var nick = encodeURI(message.guild.members.get(message.author.id).nickname);
     	message.channel.send('Смотри свою стату здесь: http://aces.lol-info.ru/s/'+nick);
+
+    	
     	var url = 'http://aces.lol-info.ru';
-    	data = gettext(url);
+
+    	var http = require('http');
+		var options = {
+		    host: url,
+		    path: '/'
+		}
+		var request = http.request(options, function (res) {
+		    var data = '';
+		    res.on('data', function (chunk) {
+		        data += chunk;
+		    });
+		    res.on('end', function () {
+		    });
+		}); 
+		 request.end();
+    	 
+    		
+    	data = data.toString();
 	    message.channel.send('Смотри свою стату: '+data);
     
   	}
